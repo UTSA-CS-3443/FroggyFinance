f3a4bb66b1ab9d71f93fab045080f8144e64a9d2
Lauren Hinnen <rgirla@icloud.com>
2025-11-10 20:13:32 -0600

    Okay Messy but yes

--- froggyFinance/src/main/java/edu/utsa/cs3443/froggyfinance/Launcher.java
+++ froggyFinance/src/main/java/edu/utsa/cs3443/froggyfinance/Launcher.java
@@ -1,11 +1,13 @@
 package edu.utsa.cs3443.froggyfinance;
 
 import javafx.application.Application;
+import edu.utsa.cs3443.froggyfinance.HelloApp;
 
 
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
+        Application.launch(HelloApp.class, args);
     }
 }
 
