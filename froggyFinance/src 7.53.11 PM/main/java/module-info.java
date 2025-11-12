a852fae089b33f52eb6e9251d6732f1fc3d8c79a
Lauren Hinnen <rgirla@icloud.com>
2025-11-12 10:35:18 -0600

    YESSSSS

--- froggyFinance/src 7.53.11 PM/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module lab2apps.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens lab2apps.calculator to javafx.fxml;
-    exports lab2apps.calculator;
-}
