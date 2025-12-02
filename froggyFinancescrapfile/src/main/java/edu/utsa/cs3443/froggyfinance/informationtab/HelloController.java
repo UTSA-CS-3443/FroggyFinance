43fabc8729c67d4e00849e9f010d90be7ae4f7db
VBaladez <victoria.a.baladez@gmail.com>
2025-12-02 13:47:05 -0600

    JavaDoc Comments to Hello Controller coding

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/HelloController.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/HelloController.java
@@ -1,12 +1,26 @@
-package edu.utsa.cs3443.froggyfinance.informationtab;
+package edu.utsa.cs3443.informationtab;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * A simple JavaFX controller used for FXML-based UI testing or demos.
+ * <p>
+ * This controller responds to a button click by updating a label’s text.
+ * It serves primarily as a template or placeholder controller.
+ */
 public class HelloController {
+
+    /** Reference to the label defined in the FXML file. */
     @FXML
     private Label welcomeText;
 
+    /**
+     * Handles the "Hello" button click event.
+     * <p>
+     * When triggered, the method updates the {@code welcomeText} label
+     * with a greeting message.
+     */
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
