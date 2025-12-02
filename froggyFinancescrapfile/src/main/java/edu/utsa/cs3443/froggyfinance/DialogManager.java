cc19e2dbba70c99335326e1b2e563458fe2bdb73
erikaMey <mey.erika@protonmail.com>
2025-12-02 14:04:02 -0600

    Added last of JavaDoc  DialogManager.java
    Should be the last of my JavaDoc! I'm good to turn in!

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/DialogManager.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/DialogManager.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Manages the dialog
+ * Manages the dialog per level, separating them into opening dialogs and
+ * feedback dialogs
  *
  * @author erikamey
  * @since 10/30/25
@@ -14,12 +15,21 @@ public class DialogManager {
     private List<Dialog> openingDialogs;
     private List<Dialog> feedbackDialogs;
     private int openIndex = 0;
-
-
+    /**
+     * Constructs a new DialogManager and initializes the lists for
+     * opening and feedback dialogs.
+     */
     public DialogManager(){
         openingDialogs = new ArrayList<>();
         feedbackDialogs = new ArrayList<>();
     }
+    /**
+    * Loads dialogs from a specified file path for a given level, clearing
+    * any previously loaded dialogs.
+    *
+    * @param filePath The path to the file containing the dialog data.
+    * @param level The level identifier for which to load the dialogs.
+    */
     public void loadDialogsForLevel(String filePath, int level){
         List<Dialog> allDialogs = DialogLoader.loadDialog(filePath, level);
 
@@ -33,7 +43,12 @@ public class DialogManager {
         }
         openIndex = 0;
     }
-
+    /**
+     * Gets the next available dialog from the openingDialogs list
+     * and increments the internal index.
+     *
+     * @return The next Dialog in the sequence.
+     */
     public Dialog getNextOpeningDialog() {
         if (openIndex < openingDialogs.size()) {
             Dialog next = openingDialogs.get(openIndex);
@@ -42,12 +57,21 @@ public class DialogManager {
         }
         return null;
     }
+    /**
+     * Retrieves a specific feedback dialog based on an index.
+     *
+     * @param questionIndex The zero-based index of the desired feedback dialog.
+     * @return The Dialog containing the right/wrong feedback.
+     */
     public Dialog getFeedbackDialog(int questionIndex){
         if (questionIndex >= 0 && questionIndex < feedbackDialogs.size()) {
             return feedbackDialogs.get(questionIndex);
         }
         return null;
     }
+    /**
+     * Resets the index used for retrieving opening dialogs.
+     */
     public void resetOpenDialogs(){
        openIndex = 0;
     }
