2e437cd89f1da979400dc38f24f976fac5f03647
VBaladez <victoria.a.baladez@gmail.com>
2025-12-02 14:19:40 -0600

    fixing mistakes

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/LevelContentScreen.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/LevelContentScreen.java
@@ -2,6 +2,11 @@ package edu.utsa.cs3443.froggyfinance.informationtab;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
