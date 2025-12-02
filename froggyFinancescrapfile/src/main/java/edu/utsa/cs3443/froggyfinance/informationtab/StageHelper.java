7f676d09f2378656f3896b4238a01a4a04e0710f
VBaladez <victoria.a.baladez@gmail.com>
2025-12-02 13:52:08 -0600

    JavaDoc Comments to Stage Helper coding

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/StageHelper.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/StageHelper.java
@@ -1,4 +1,4 @@
-package edu.utsa.cs3443.froggyfinance.informationtab;
+package edu.utsa.cs3443.informationtab;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -6,7 +6,25 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * Utility class that provides simple helper methods for displaying
+ * small modal message windows.
+ * <p>
+ * This is typically used in place of traditional dialog boxes to show short
+ * alerts, notifications, or informational messages within the JavaFX
+ * application.
+ */
 public class StageHelper {
+
+    /**
+     * Displays a small modal pop-up window containing a message.
+     * <p>
+     * The window is created with {@link Modality#APPLICATION_MODAL}, meaning it
+     * blocks interaction with other application windows until closed.
+     *
+     * @param title   the title of the message window
+     * @param message the text content displayed inside the window
+     */
     public static void showMessage(String title, String message) {
         Stage stage = new Stage();
         stage.setTitle(title);
