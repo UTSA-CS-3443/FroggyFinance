3ba57d37e827c36267010ec3ee4e970b400af39b
VBaladez <victoria.a.baladez@gmail.com>
2025-12-02 13:55:44 -0600

    JavaDoc comment to Main Screen coding

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/MainScreen.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/MainScreen.java
@@ -9,8 +9,26 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import edu.utsa.cs3443.froggyfinance.FloatingMenu;
 
+/**
+ * Represents the main menu screen of the FroggyFinance application.
+ * <p>
+ * This screen provides navigation to the Information section or allows
+ * the user to return to the {@link FloatingMenu}.
+ */
 public class MainScreen {
 
+    /**
+     * Displays the main menu in the provided {@link Stage}.
+     * <p>
+     * The menu contains:
+     * <ul>
+     *     <li>A header label showing "Main Menu"</li>
+     *     <li>A button to navigate to the {@link InformationMenu}</li>
+     *     <li>A button to return to the {@link FloatingMenu}</li>
+     * </ul>
+     *
+     * @param stage the primary JavaFX window where the main menu is displayed
+     */
     public void start(Stage stage) {
         stage.setTitle("Main Menu");
 
@@ -26,16 +44,23 @@ public class MainScreen {
         exitButton.setPrefSize(260, 36);
         exitButton.setFont(Font.font(14));
 
+        // Navigate to the InformationMenu
         infoButton.setOnAction(e -> new InformationMenu().show(stage));
 
-        // Go back to Floating Menu
+        // Return to the FloatingMenu
         exitButton.setOnAction(e -> FloatingMenu.open(stage));
 
         VBox root = new VBox(20, title, infoButton, exitButton);
         root.setAlignment(Pos.CENTER);
-        root.setStyle("-fx-padding: 30; -fx-background-color: linear-gradient(to bottom, #a0e3f0, #60c4d8); -fx-border-radius: 15; -fx-background-radius: 15;");
+        root.setStyle(
+            "-fx-padding: 30; " +
+            "-fx-background-color: linear-gradient(to bottom, #a0e3f0, #60c4d8); " +
+            "-fx-border-radius: 15; " +
+            "-fx-background-radius: 15;"
+        );
 
         stage.setScene(new Scene(root, 420, 300));
         stage.show();
     }
 }
+
