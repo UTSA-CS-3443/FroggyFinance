3dc909921537ee0abdc930c7b0b06dc4a61b906a
VBaladez <victoria.a.baladez@gmail.com>
2025-12-02 14:33:18 -0600

    adding author name

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/MainScreen.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/informationtab/MainScreen.java
@@ -14,6 +14,7 @@ import edu.utsa.cs3443.froggyfinance.FloatingMenu;
  * <p>
  * This screen provides navigation to the Information section or allows
  * the user to return to the {@link FloatingMenu}.
+ * @author Victoria Baladez
  */
 public class MainScreen {
 
