4449424387d3607c90fea81dc22474c808e6a43a
LaurenHinnen <lauren.hinnen@my.utsa.edu>
2025-12-03 15:20:44 -0600

    Update GameLauncher.java

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/GameLauncher.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/GameLauncher.java
@@ -1,13 +1,10 @@
 package edu.utsa.cs3443.froggyfinance;
 
 import javafx.application.Application;
-import edu.utsa.cs3443.froggyfinance.HelloApp;
-
 
 public class GameLauncher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-        Application.launch(HelloApp.class, args);
+        Application.launch(GameApplication.class, args);
     }
 }
 
