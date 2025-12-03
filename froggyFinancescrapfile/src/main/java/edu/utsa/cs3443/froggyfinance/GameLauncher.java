f6291bd55146935b99dc367167c342339bf9204f
LaurenHinnen <lauren.hinnen@my.utsa.edu>
2025-12-03 14:51:49 -0600

    Update and rename Launcher.java to GameLauncher.java

--- froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/GameLauncher.java
+++ froggyFinancescrapfile/src/main/java/edu/utsa/cs3443/froggyfinance/GameLauncher.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import edu.utsa.cs3443.froggyfinance.HelloApp;
 
 
-public class Launcher {
+public class GameLauncher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
         Application.launch(HelloApp.class, args);
